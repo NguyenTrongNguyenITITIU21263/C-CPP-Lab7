6bcdb878fa2d1c7563fd8943ee758549f118d87d
NgienKem <118713716+NguyenTrongNguyenITITIU21263@users.noreply.github.com>
2022-11-26 17:40:37 +0700

    Update Exercise1.c

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -15,7 +15,24 @@ If there is no prefix, just print “Prefix not found!”
 #define SIZE 1000
 
 void Ex1(char *str[], int n){
-	//your codes here
+size_t findprefix( const char *str1, const char *str2 )
+{
+    size_t n = 0;
+    for ( ; *str1 && *str1 == *str2; ++str1, ++str2 )
+    {
+        ++n;
+    }
+    return n;
+}
+
+int main( void ) 
+{
+    const char *str1 = "abc";
+    const char *str2 = "abd";
+    size_t n = findprefix( str1, str2 );
+    if ( n != 0 ) printf( "\"%.*s\"\n", ( int )n, str1 );
+    return 0;
+}
 	
 }
 
