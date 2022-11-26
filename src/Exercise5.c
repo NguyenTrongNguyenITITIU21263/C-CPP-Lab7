adbd6654c655403aea156b49e0a612f41f2504ad
NgienKem <118713716+NguyenTrongNguyenITITIU21263@users.noreply.github.com>
2022-11-26 17:40:55 +0700

    Update Exercise5.c

--- src/Exercise5.c
+++ src/Exercise5.c
@@ -16,11 +16,36 @@ Ex:
 #include<limits.h>
 #define SIZE 1000
 
-void Ex5(char s[]){
+void Ex5(char s[])
+{
 	//your codes here
-	
+	int n = strlen(s);
+    int digitArray[n];
+    for(int i=0;i<n;i++)
+    digitArray[i]=s[i]-'0';
+    char result[n + 1];
+    result[n] = '\0';
+    printAllWords(digitArray, 0, result, n); 
 }
-
+const char phoneTable[10][5]= {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz" };            
+void printAllWords(int s[],int curr_digit,char output[],int n) 
+{    
+    int i;
+    if (curr_digit == n) 
+	{
+		printf("%s ", output);
+		return; 
+    }
+	for (i = 0; i < strlen(phoneTable[s[curr_digit]]);i++)      
+    {
+		output[curr_digit]= phoneTable[s[curr_digit]][i];
+		printAllWords(s, curr_digit + 1, output, n);
+		if (s[curr_digit] == 0|| s[curr_digit] == 1)
+		{
+			return;
+		}	
+    } 
+}  
 int main(int argc, char *argv[]) {
 	char *s = argv[1];
     Ex5(s);
