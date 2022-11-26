49cbb9b0916b26ae8d6686cd53a3e5078d0064c6
NgienKem <118713716+NguyenTrongNguyenITITIU21263@users.noreply.github.com>
2022-11-26 17:40:48 +0700

    Update Exercise3.c

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -17,7 +17,110 @@ Ex:
 
 void Ex3(char s1[], char s2[]){
 	//your codes here
-	
+		int s1Size = strlen(s1);     
+	int s2Size = strlen(s2);
+	int real1 = 0,real2 = 0,img1 = 0,img2 = 0;
+	int flag1 = 1;     
+	int flag2 = 1;
+	for(int i = 0; i < s1Size; i++) 
+	{
+		if(s1[i] == '-') 
+		{
+			if(flag1 == 1)
+			{
+				flag1 = 0;
+			}else 
+			{
+				flag2 = 0;
+			} 
+		}
+		if(s1[i]== '+') 
+		{
+			flag1 =1;
+			flag2 =0;
+			if( s1[i] >= '0' && s1[i] <= '9')
+			{
+				if (flag2 == 1)
+				{
+					if(real1 < 0)
+					{
+						real1 *= -1;
+						real1 *= 10;
+						real1 += s1[i]- '0';
+						if (flag1 == 0 && flag2 == 1)
+						{
+							real1 *= -1;
+						}
+					}
+				}else
+				{
+					if(img1 < 0)
+					{
+						img1 *= -1;
+						img1 *= 10;
+						img1 += s1[i] - '0';
+						if (flag1 == 0 && flag2 == 0)
+						{
+							img1 *= -1;
+						}
+					}
+				}
+			}
+		}
+	}
+	for (int i = 0; i < s2Size; i++)
+	{
+		if(s2[i] == '-')
+		{
+			if(flag1 == 1)
+			{
+				flag1 = 0;
+			}else
+			{
+				flag2 = 0;
+			}	
+		}
+		if(s2[i] == '+')
+		{
+			flag1 = 1;
+			flag2 = 0;
+			if (s2[i]>= '0' && s2[i] <= '9')
+			{
+				if(flag2 == 1)
+				{
+					if (real2 < 0)
+					real2 *= -1;
+					real2 *= 10;
+					real2 += s2[i] - '0';
+					if(flag1 == 0 && flag2 == 1)
+					{
+						real2 *= -1;
+					}
+				}else
+				{
+					if(img2 < 0)
+					{
+						img2 *= -1;
+						img2 *= 10;
+						img2 += s2[i] - '0';
+						if(flag1 == 0 && flag2 == 0)
+						{
+							img2 *= -1;
+						}
+					}
+				}
+			}
+		}
+	}
+	int realRes = real1 * real2 - (img1*img2);
+	int imgRes = real1 * img2 + real2 * img1;
+	if(imgRes >= 0)
+	{
+		printf("%d+%di\n", realRes,imgRes);
+	}else
+	{
+		printf("%d%di\n",realRes,imgRes);
+	}	
 }
 
 int main(int argc, char *argv[]) {
